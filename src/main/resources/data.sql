ddff9bb6fecce4fdef4369ace75c2335c122318d
tarik mamou <>
2021-09-01 14:50:58 +0000

    premier commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tarik (id, username, password) VALUES
+  (1, 'usr1', 'bigSecret'),
+  (2, 'usr2', 'topSecret');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+
+  CREATE TABLE tarik (
+    id          INT PRIMARY KEY,
+    username    VARCHAR(64) NOT NULL,
+    password    VARCHAR(64)
+  );
