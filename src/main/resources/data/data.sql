b700ae2d56e3e2733614afad91012c512fdd1178
tarik mamou <>
2021-09-05 12:17:04 +0000

    plus de tables

--- src/main/resources/data/data.sql
+++ src/main/resources/data/data.sql
@@ -11,30 +11,37 @@ INSERT INTO auteur (id, nom, prenom)  VALUES
  ( 1,'auteur1', 'prenom1'),
   ( 2,'auteur2', 'prenom2');
 
-INSERT INTO livre (id, nom, langue, id_auteur, id_profile) VALUES
-  ( 1, 'une vie','fr',1,1),
-  ( 2, 'crimes et chatiments','rs',2,2);
-
-  COMMIT;
+INSERT INTO livre (id, nom, langue, prix, id_auteur, id_profile,id_stock) VALUES
+  ( 1, 'une vie','fr',100,1,1,1),
+  ( 2, 'crimes et chatiments','rs',200, 2,2,2);
 
 
-  id INT PRIMARY KEY,
-  nom         VARCHAR(64) NOT NULL,
-  description   VARCHAR(64) );
-  -- secuity
+
+INSERT INTO stock (id, nombre) VALUES
+  ( 1, 20),
+  ( 2, 30);
+
+
+  -- security
 
 
 INSERT INTO role (id, nom, description) VALUES
-  ( 1, 'admin','tous le droits '),
-  ( 2, 'user','droits restreints');
+    ( 1, 'admin','tous le droits '),
+    ( 2, 'user','droits restreints');
 
-  INSERT INTO user (id, nom, prenom, mail, actif) VALUES
-    ( 1, 'alaoui','ahmed'),
-    ( 2, 'maani','ali'),
-    ( 3, 'fehli','ali');
 
-       INSERT INTO user_role (id_user, id_role) VALUES
-            ( 1, 1),
-            ( 2, 2),
-            ( 3, 2) ;
+INSERT INTO user (id, username, nom, prenom, password, mail, actif) VALUES
+--pasword1
+        ( 1,'username_1', 'alaoui','ahmed','$2a$10$iU0hTb92bnYIixazpeTWke3/sAphcYqdhDR1q0zgSX8v7nkTipDgW','alaoui_ahmed@gmail.com',TRUE),
+        --pasword2
+    ( 2, 'username_2','maani','ali','$2a$10$CrJptyJI/5wd7MRW7Ze6E.o6aTrjHXI7yzICFrp8Yc3vfl5whZlk2','maani_ali@gmail.com',TRUE),
+    --pasword3
+    ( 3, 'username_3','fdali','sara','$2a$10$r.BPz3MgsTfqZRMultVA8.NmUe5pL3JCONapJ5Pc4bMLbQHijnG9.','fdali_sara@gmail.com',TRUE);
+
+INSERT INTO user_role (id_user, id_role) VALUES
+    ( 1, 1),
+    ( 2, 2),
+    ( 3, 2) ;
+
+COMMIT;
 
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,6 @@
+  set autocommit false;
+
+
   CREATE TABLE  IF NOT EXISTS auteur (
        id   INT PRIMARY KEY,
        nom VARCHAR(64) NOT NULL,
@@ -12,28 +15,40 @@
 
 
 
-  CREATE TABLE IF NOT EXISTS  livre (
-    id          INT PRIMARY KEY ,
-    nom         VARCHAR(64) NOT NULL,
-    langue      VARCHAR(64),
-    id_auteur INT  NOT NULL ,
-    id_profile  INT  NOT NULL,
 
-    CONSTRAINT FK_livre_auteur FOREIGN KEY (id_auteur) REFERENCES auteur(id),
-     CONSTRAINT FK_livre_profile FOREIGN KEY (id_profile) REFERENCES profil(id)
-  );
+      CREATE TABLE IF NOT EXISTS  livre (
+               id          INT PRIMARY KEY ,
+               nom         VARCHAR(64) NOT NULL,
+               langue      VARCHAR(64),
+               prix         INT NOT NULL,
+               id_auteur INT  NOT NULL ,
+               id_profile  INT  NOT NULL,
+               id_stock   INT NOT NULL,
 
--- securite
+
+               CONSTRAINT FK_livre_auteur FOREIGN KEY (id_auteur) REFERENCES auteur(id),
+                CONSTRAINT FK_livre_profile FOREIGN KEY (id_profile) REFERENCES profil(id),
+                CONSTRAINT FK_livre_stock FOREIGN KEY (id_stock) REFERENCES stock(id)
+             );
+
+
+commit;
+-- security
 CREATE TABLE IF NOT EXISTS  user (
 
 id INT PRIMARY KEY,
+username  VARCHAR(64) NOT NULL,
 nom         VARCHAR(64) NOT NULL,
-prenom  VARCHAR(64) NOT NULL );
+prenom  VARCHAR(64) NOT NULL ,
+password VARCHAR(64) NOT NULL,
+mail VARCHAR(64) NOT NULL,
+actif  BOOLEAN NOT NULL DEFAULT TRUE
+);
 
 
 CREATE TABLE IF NOT EXISTS  role (
 
-id INT PRIMARY KEY,
+id INT PRIMARY KEY ,
 nom         VARCHAR(64) NOT NULL,
 description   VARCHAR(64) );
 
@@ -45,7 +60,7 @@ id_role INT ,
 
 CONSTRAINT FK_user_role_role FOREIGN KEY (id_role) REFERENCES role(id) ,
 CONSTRAINT FK_user_role_user FOREIGN KEY (id_user) REFERENCES user(id)
-)
+);
 
 
 
@@ -53,29 +68,28 @@ CONSTRAINT FK_user_role_user FOREIGN KEY (id_user) REFERENCES user(id)
 
 
 
+CREATE TABLE  IF NOT EXISTS  user_livre  (
+   id_livre INT NOT NULL ,
+   id_user INT NOT NULL  ,
+ CONSTRAINT FK_user_livre_livre FOREIGN KEY (id_livre) REFERENCES livre (id),
+     CONSTRAINT FK_user_livre_user FOREIGN KEY (id_user) REFERENCES user (id)
+
+);
+
+CREATE TABLE IF NOT EXISTS  stock  (
+   id INT PRIMARY KEY ,
+   nombre INT NOT NULL default 0,
+);
 
 
+  CREATE TABLE IF NOT EXISTS  action (
+
+  id INT PRIMARY KEY,
+  commentaire VARCHAR(64),
+  date_action DATE NOT NULL,
+  id_user  VARCHAR(64) NOT NULL ,
+  CONSTRAINT FK_action FOREIGN KEY (id_user) REFERENCES user(id)
+  );
 
 
-
-
-
-
-
-
-  --optionel many to_many
- --CREATE TABLE  lecteur {
-  --       id   INT PRIMARY KEY,
-   --      nom VARCHAR(64);
--- }
-
-
---  CREATE TABLE  livre_lecteur {
---    id_livre INT NOT NULL FOREIGN KEY  REFERENCES livre(id),
---
---    id_lecteur INT NOT NULL  FOREIGN KEY  REFERENCES lecteur (id),
---
---     CONSTRAINT [PK_livre_lecteur] PRIMARY KEY (
---        id_livre,id_lecteur
---     )
---  }
+commit;
